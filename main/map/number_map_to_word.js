6ec4189abcc9eda287360b4cb0eefb43dc652193
mizhidan <dj9824@126.com>
2020-01-19 16:06:37 +0800

    [danjie] feat: complete /maim/map

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,9 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map((item) => get_letter_interval(item))
 };
 
+function get_letter_interval(number_a) {
+  return String.fromCharCode(96 + number_a);
+}
 module.exports = number_map_to_word;
--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,14 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map((item) => get_letter_interval_2(item))
 };
+
+function get_letter_interval_2(number_a) {
+  if(number_a < 26) {
+    return String.fromCharCode(96 + number_a);
+  } else {
+    return String.fromCharCode(96 + number_a / 26) + String.fromCharCode(96 + number_a % 26);
+  }
+}
 
 module.exports = number_map_to_word_over_26;
