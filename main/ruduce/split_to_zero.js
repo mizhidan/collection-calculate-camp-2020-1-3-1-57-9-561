aec9a7ecc6cba9627a3a06e9f0964c5598718004
mizhidan <dj9824@126.com>
2020-01-19 18:00:31 +0800

    [danjie] feat: complete /main/ruduce/

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let resArr = [];
+  if ((number / interval) % 1 === 0) {
+    for (let res = number; res >= 0; res -= interval) {
+      if(res === 0) {
+        resArr.push(parseInt(res));
+      } else {
+        resArr.push(parseFloat(res.toFixed(1)));
+      }
+    }
+  } else {
+    for (let res = number; res >= (0 - interval); res -= interval) {
+      resArr.push(parseFloat(res.toFixed(1)));
+    }
+  }
+  return resArr;
 }
 
 module.exports = spilt_to_zero;
